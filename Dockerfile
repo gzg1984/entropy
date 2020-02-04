bc0acb213b830177fd01520b1104be70c2a15e42
Zhigang Gao <gzg1984@aliyun.com>
2020-02-04 17:30:32 +0800

    first Docker file and creater

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM gzg1984/dev_ubuntu:latest
 LABEL maintainer="Maxpain <g.zg1984@gmail.com>"
-RUN git clone git@github.com:gzg1984/entropy.git
+RUN git clone http://github.com/gzg1984/entropy.git
 WORKDIR /entropy
 EXPOSE 22
 EXPOSE 8888
--- createDockerfile.sh
+++ createDockerfile.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-PROJECT_TARGET=`git remote -v|head -n1|awk '{print $2}'`
-PROJECT=`basename ${PROJECT_TARGET}`
+SSH_TARGET=`git remote -v|head -n1|awk '{print $2}'`
+PROJECT=`basename ${SSH_TARGET}`
 DIR=`echo $PROJECT|cut -d"." -f1`
 
 cat <<! > Dockerfile
 FROM gzg1984/dev_ubuntu:latest
 LABEL maintainer="Maxpain <g.zg1984@gmail.com>"
-RUN git clone ${PROJECT_TARGET}
+RUN git clone http://github.com/gzg1984/${PROJECT}
 WORKDIR /${DIR}
 EXPOSE 22
 EXPOSE 8888
